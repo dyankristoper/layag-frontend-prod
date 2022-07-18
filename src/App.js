785a96415589da07b081c0b7d2c5453846f2c5c7
Jarren <vince.dehonor1@gmail.com>
2022-07-19 03:42:40 +0800

    Feat: Login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import "./App.scss";
 import Home from "./views/Home";
+import Login from "./views/Login"
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-        <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
     </div>
   );
 }
---
+++ src/views/Login.js
@@ -0,0 +1,32 @@
+import layag from "../components/Images/layag-icon.png";
+import './Login.scss';
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-information">
+        <Link to="/">
+          <img className="login-information__logo" src={layag} alt="" />
+        </Link>
+        <h3>Welcome back</h3>
+        <p className="login-information__details">Please enter your details.</p>
+        <input className="login-information__input" type="email" placeholder="Enter your email" />
+        <p></p>
+        <input className="login-information__input" type="password" placeholder="Enter your password" />
+        <p className="login-information__details">Forgot password?</p>
+        <button type="button" className="login-information__btn"> Proceed </button>
+        <p>Don't have an account?<span>Sign up</span></p>
+        <p className="login-information__copyright">All images and information is for educational background purposes only © 2022</p>
+      </div>
+      <div className="login-image">
+        <div className="image">
+
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Login;
