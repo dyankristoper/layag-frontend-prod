123fa1d2c534d88260f647570a673cceaf2e1a14
Jarren <vince.dehonor1@gmail.com>
2022-07-19 07:30:17 +0800

    Fix: Login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import "./App.scss";
 import Home from "./views/Home";
 import Login from "./views/Login"
+import SignUp from "./views/SignUp";
 import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
 
     </div>
