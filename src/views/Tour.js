9a95e9308635410a6c98c04135f6a4bf04c23f01
James Anuncio <anunciojames14@gmail.com>
2022-07-21 03:59:27 +0800

    Feat: connected database to frontend and mapped the data

--- src/views/Tour.js
+++ src/views/Tour.js
@@ -8,6 +8,7 @@ import bg from "date-fns/esm/locale/bg/index.js";
 
 const Tour = () => {
   const [tours, setTours] = useState([]);
+  const [results, setResult] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -18,11 +19,9 @@ const Tour = () => {
         );
 
         const imageCovers = getData.data.data.tours.map((el) => el.imageCover);
-        console.log(imageCovers);
-
-        // console.log(getData.data.data);
-        // setTours(getData.data.data.tours);
-    
+        console.log(getData.data.data.tours);
+        setTours(getData.data.data.tours);
+        setResult(getData.data);
       } catch (err) {
         console.log(err);
       }
@@ -31,6 +30,8 @@ const Tour = () => {
     getTours();
   }, [id]);
 
+  console.log(tours);
+
   return (
     <>
       <Header />
@@ -38,7 +39,7 @@ const Tour = () => {
         <div className="Tour__hero"></div>
         <div className="Tour__Title">
           <h3>
-            5 Tours Found In <span>Cebu</span>
+            {results.results} Tours Found In <span>{id[0].toUpperCase() + id.substring(1)}</span>
           </h3>
         </div>
 
@@ -95,92 +96,54 @@ const Tour = () => {
             </div>
           </div>
           <div className="details-section">
-            <div className="details-section__choices">
-              <div className="image">
-                <img
-                  src="https://pbs.twimg.com/media/DV5hSLkXcAAEj12.jpg:large"
-                  alt="kawasan"
-                />
-              </div>
-              <div className="details">
-                <h3>4 Days 3 Nights South of Cebu Tour</h3>
+            {tours &&
+              tours.map((t) => {
+                return (
+                  <div className="details-section__choices" key={t.id}>
+                    <div className="image">
+                      <img
+                        src={t.images[0]}
+                        alt={t.name}
+                      />
+                    </div>
+                    <div className="details">
+                      <h3>{t.name}</h3>
 
-                <div class="stars">
-                  <svg viewBox="0 0 576 512" width="100" title="star">
-                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
-                  </svg>
-                  <svg viewBox="0 0 576 512" width="100" title="star">
-                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
-                  </svg>
-                  <svg viewBox="0 0 576 512" width="100" title="star">
-                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
-                  </svg>
-                  <svg viewBox="0 0 576 512" width="100" title="star">
-                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
-                  </svg>
-                  <svg viewBox="0 0 576 512" width="100" title="star">
-                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
-                  </svg>
+                      <div class="stars">
+                        <svg viewBox="0 0 576 512" width="100" title="star">
+                          <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+                        </svg>
+                        <svg viewBox="0 0 576 512" width="100" title="star">
+                          <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+                        </svg>
+                        <svg viewBox="0 0 576 512" width="100" title="star">
+                          <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+                        </svg>
+                        <svg viewBox="0 0 576 512" width="100" title="star">
+                          <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+                        </svg>
+                        <svg viewBox="0 0 576 512" width="100" title="star">
+                          <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+                        </svg>
 
-                  <span className="TourPackages-list__rate"> 5</span>
-                  <span className="TourPackages-list__review">
-                    ( 150 reviews )
-                  </span>
-                </div>
-                <h4>Details</h4>
+                        <span className="TourPackages-list__rate"> 5</span>
+                        <span className="TourPackages-list__review">
+                          ( 150 reviews )
+                        </span>
+                      </div>
+                      <h4>Price: {t.price}</h4>
 
-                <p>This elegant, 19th-century building overlooks Lake Maggiore. You will find every comfort for a relaxing break including a swimming pool, gym, and games room.</p>
+                      <p>
+                        {t.summary}
+                      </p>
 
-                <h4>Distance:</h4>
+                      <h4>Distance:</h4>
 
-                <p>10km</p>
-
-              </div>
-            </div>
-
-            <div className="details-section__choices">
-              <div className="image">
-                <img
-                  src="https://pbs.twimg.com/media/DV5hSLkXcAAEj12.jpg:large"
-                  alt="kawasan"
-                />
-              </div>
-              <div className="details">
-                <h3>4 Days 3 Nights South of Cebu Tour</h3>
-
-                <div class="stars">
-                  <svg viewBox="0 0 576 512" width="100" title="star">
-                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
-                  </svg>
-                  <svg viewBox="0 0 576 512" width="100" title="star">
-                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
-                  </svg>
-                  <svg viewBox="0 0 576 512" width="100" title="star">
-                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
-                  </svg>
-                  <svg viewBox="0 0 576 512" width="100" title="star">
-                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
-                  </svg>
-                  <svg viewBox="0 0 576 512" width="100" title="star">
-                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
-                  </svg>
-
-                  <span className="TourPackages-list__rate"> 5</span>
-                  <span className="TourPackages-list__review">
-                    ( 150 reviews )
-                  </span>
-                </div>
-                <h4>Details</h4>
-
-                <p>This elegant, 19th-century building overlooks Lake Maggiore. You will find every comfort for a relaxing break including a swimming pool, gym, and games room.</p>
-
-                <h4>Distance:</h4>
-
-                <p>10km</p>
-
-              </div>
-            </div>
-
+                      <p>10km</p>
+                    </div>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </div>
