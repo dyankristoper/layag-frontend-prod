4cc9df8e44fc9cc911167ba338ac095d7f1fd07a
Jarren <vince.dehonor1@gmail.com>
2022-07-22 01:57:37 +0800

    Feat: added ratings and reviews

--- src/views/TourDetails.js
+++ src/views/TourDetails.js
@@ -7,7 +7,9 @@ import Header from '../components/Header';
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-
+import { DateRange } from "react-date-range";
+import { addDays } from 'date-fns'
+import format from 'date-fns/format';
 
 const TourDetails = () => {
 
@@ -16,6 +18,14 @@ const TourDetails = () => {
   const [tourEndingLocation, setTourEndingLocation] = useState();
   const [tourImages, setTourImages] = useState([]);
   const { id } = useParams();
+
+  const [range, setRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 1),
+      key: 'selection'
+    }
+  ])
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -56,13 +66,13 @@ const TourDetails = () => {
         <div className="Tour-Details">
           <div className="Tour-Details__images">
             <div className="Tour__image">
-              <img src={tourImages[0]} alt="image" />
+              <img src={tourImages[0]} alt="Tour" />
             </div>
             <div className="Tour__images">
-              <div><img src={tourImages[1]} alt="image1" /></div>
-              <div><img src={tourImages[2]} alt="image2" /></div>
-              <div><img src={tourImages[3]} alt="image3" /></div>
-              <div><img src={tourImages[4]} alt="image4" /></div>
+              <div><img src={tourImages[1]} alt="Tour image1" /></div>
+              <div><img src={tourImages[2]} alt="Tour image2" /></div>
+              <div><img src={tourImages[3]} alt="Tour image3" /></div>
+              <div><img src={tourImages[4]} alt="Tour image4" /></div>
             </div>
           </div>
           <div className="Tour-Details__documentation">
@@ -106,10 +116,39 @@ const TourDetails = () => {
                 <p>{tour.description}</p>
               </div>
             </div>
-            <div className="Tour-Details__documentation-date"></div>
+            <div className="Tour-Details__documentation-date">
+              <div className="calendarWrap" >
+                <DateRange
+                  onChange={item => setRange([item.selection])}
+                  editableDateInputs={true}
+                  moveRangeOnFirstSelection={false}
+                  ranges={range}
+                  months={1}
+                  direction="horizontal"
+                  className="calendarElement"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="Reviews">
+          <h3>Reviews</h3>
+          <div className="Tour-Review">
+            <div className="Tour-Review__image">
+              <img src="https://www.musicmundial.com/en/wp-content/uploads/2022/07/BLACKPINKs-Jennie-saves-a-store-from-bankruptcy-just-by-posting-it-on-Instagram-1140x795.jpg" alt="Jennie" />
+            </div>
+            <div className="Tour-Review__details">
+              <p className="Tour-Review__details__name">Kathleen Sy</p>
+              <p className="Tour-Review__details__rating"></p>
+              <p className="Tour-Review__details__date">23/05/2022</p>
+              <p className="Tour-Review__details__comment">Great activity. Gives you the chance to explore and learn about the lesser-popular but just as beautiful attractions of Coron. I recommend doing this activity along with Coron's famous water attractions to get a full appreciation of the island. Our guide was extremely helpful and very informative. He clearly knew what he was talking about. Definitely recommend.</p>
+            </div>
           </div>
         </div>
       </div>
+
+
     </>
   )
 }
