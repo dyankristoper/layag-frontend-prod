841feb32eb1b1dc6bedb2be470a30d150f3a8891
Jarren <vince.dehonor1@gmail.com>
2022-07-23 05:54:43 +0800

    Fix: logo link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import './Header.scss';
 import layag from './Images/layag-icon.png';
 import profile from './Images/user-menu64.png';
-import {useState} from 'react';
-import {Link} from "react-router-dom";
+import { useState } from 'react';
+import { Link } from "react-router-dom";
 
 const Header = () => {
 
@@ -14,33 +14,35 @@ const Header = () => {
 
   return (
     <>
-    
-    <header className="Header">
-      <div className="Header__logoTitle">
-        <img className="Header__logo" src={layag} alt="logo" />
-        <h1>Layag</h1>
-      </div>
 
-      <div className="Header__userSection">
-        <img src={profile} alt="user-login" onClick ={onclick} />
-      </div>
-    
-  
+      <header className="Header">
+        <Link to="/">
+          <div className="Header__logoTitle">
+            <img className="Header__logo" src={layag} alt="logo" />
+            <h1>Layag</h1>
+          </div>
+        </Link>
 
-    {/* // MODAL */}
-    {
-      modal && 
-      <div className='Header__modal'>
-        <ul>
-          <Link to="/profile"><li>Profile</li></Link>
-          <li>Tour</li>
-          <li>Help</li>
-        </ul>
-      
-      </div>
-    }
+        <div className="Header__userSection">
+          <img src={profile} alt="user-login" onClick={onclick} />
+        </div>
 
-    </header>
+
+
+        {/* // MODAL */}
+        {
+          modal &&
+          <div className='Header__modal'>
+            <ul>
+              <Link to="/profile"><li>Profile</li></Link>
+              <li>Tour</li>
+              <li>Help</li>
+            </ul>
+
+          </div>
+        }
+
+      </header>
     </>
   );
 };
