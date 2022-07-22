85e608dfda35613d1b9adb9263c99380fd08a2fc
Jarren <vince.dehonor1@gmail.com>
2022-07-23 05:38:00 +0800

    Fix: add and edit functionalities

--- src/views/AddEditTours.js
+++ src/views/AddEditTours.js
@@ -1,24 +1,76 @@
 import './AddEditTours.scss';
-import { useState } from 'react'
+import { useState, /* useEffect */ } from 'react'
+// import { useParams } from 'react-router'
+import axios from 'axios';
 
 const AddEditTours = () => {
 
+  // const [tourId, setTourId] = useState(null);
   const [tourName, setTourName] = useState('');
   const [tourSummary, setTourSummary] = useState('');
   const [tourDescription, setTourDescription] = useState('');
-  const [tourDuration, setTourDuration] = useState('');
-  const [tourPrice, setTourPrice] = useState('');
-  const [tourImageCover, setTourImageCover] = useState('');
-  const [tourImage1, setTourImage1] = useState('');
-  const [tourImage2, setTourImage2] = useState('');
-  const [tourImage3, setTourImage3] = useState('');
-  const [tourImage4, setTourImage4] = useState('');
-  const [tourImage5, setTourImage5] = useState('');
+  const [tourDuration, setTourDuration] = useState(0);
+  const [tourPrice, setTourPrice] = useState(0);
+  const [tourMaxGroupSize, setTourMaxGroupSize] = useState(0);
+  const [tourImage, setTourImage] = useState('');
+  const [tourDifficulty, setTourDifficulty] = useState('');
+  // const { id } = useParams();
+
+
 
   const onClick = () => {
 
+    const addingItems = async () => {
+      try {
+        // let response;
+        // if (id) {
+        //   response = await axios.put(`http://localhost:8000/api/v1/tours/${id}`, {
+        //     name: tourName, duration: tourDuration, maxGroupSize: tourMaxGroupSize, difficulty: tourDifficulty, price: tourPrice, summary: tourSummary, description: tourDescription, imageCover: tourImage
+        //   });
+        // } else {
+        const response = await axios.post('http://localhost:8000/api/v1/tours', {
+          tours: [{ name: tourName, duration: tourDuration, maxGroupSize: tourMaxGroupSize, difficulty: tourDifficulty, price: tourPrice, summary: tourSummary, description: tourDescription, imageCover: tourImage }],
+        });
 
+
+        console.log(response.data);
+
+      } catch (error) {
+        console.log(error.message);
+      }
+
+    }
+
+    addingItems();
+    console.log(tourName, tourSummary, tourDescription, tourDuration, tourPrice, tourMaxGroupSize, tourImage, tourDifficulty)
   }
+
+  // useEffect(() => {
+  //   if (id) {
+  //     const getById = async () => {
+  //       try {
+  //         const response = await axios.get(
+  //           `http://localhost:8000/api/tours/${id}`
+  //         );
+
+  //         if (response.data.data.tours) {
+  //           setTourId(response.data.data.tours._id);
+  //           setTourName(response.data.data.tours.name);
+  //           setTourSummary(response.data.data.tours.summary);
+  //           setTourDescription(response.data.data.tours.description);
+  //           setTourDuration(response.data.data.tours.duration);
+  //           setTourPrice(response.data.data.tours.price);
+  //           setTourImage(response.data.data.tours.imageCover);
+  //         }
+  //       } catch (error) {
+  //         console.log(error.message);
+  //       }
+  //     };
+
+  //     getById();
+  //   }
+  // }, [id]);
+
 
 
   return (
@@ -26,21 +78,18 @@ const AddEditTours = () => {
       <div className="container" >
         <h3><span>Add</span> new tour</h3>
         <input className="AddEdit-input" type="text" placeholder='Enter tour name' onChange={e => setTourName(e.target.value)} />
-        <textarea className="AddEdit-description AddEdit-input" placeholder='Enter summary' onChange={e => setTourSummary(e.target.value)} />
-        <textarea className="AddEdit-description AddEdit-input" placeholder='Enter description' onChange={e => setTourDescription(e.target.value)} />
+        <textarea className="AddEdit-description AddEdit-input" placeholder='Enter Summary' onChange={e => setTourSummary(e.target.value)} />
+        <textarea className="AddEdit-description AddEdit-input" placeholder='Enter Description' onChange={e => setTourDescription(e.target.value)} />
       </div>
       <div className="container">
         <div className="duration-price">
-          <input className="duration-price__input" type="text" placeholder='Enter duration' onChange={e => setTourDuration(e.target.value)} />
+          <input className="duration-price__input" type="text" placeholder='Enter Duration' onChange={e => setTourDuration(e.target.value)} />
           <input className="duration-price__input" type="text" placeholder='Enter Price' onChange={e => setTourPrice(e.target.value)} />
+          <input className="duration-price__input" type="text" placeholder='Enter Max group size' onChange={e => setTourMaxGroupSize(e.target.value)} />
+          <input className="duration-price__input" type="text" placeholder='Enter Difficulty' onChange={e => setTourDifficulty(e.target.value)} />
         </div>
-        <input className="AddEdit-input" type="text" placeholder='Enter Image cover' onChange={e => setTourImageCover(e.target.value)} />
-        <input className="AddEdit-input" type="text" placeholder='Enter first image' onChange={e => setTourImage1(e.target.value)} />
-        <input className="AddEdit-input" type="text" placeholder='Enter second image' onChange={e => setTourImage2(e.target.value)} />
-        <input className="AddEdit-input" type="text" placeholder='Enter third image' onChange={e => setTourImage3(e.target.value)} />
-        <input className="AddEdit-input" type="text" placeholder='Enter fourth image' onChange={e => setTourImage4(e.target.value)} />
-        <input className="AddEdit-input" type="text" placeholder='Enter fifth image' onChange={e => setTourImage5(e.target.value)} />
-        <button type="button" className="AddEdit-btn" onClick={onClick}>Add+</button>
+        <input className="AddEdit-input" type="text" placeholder='Enter Image cover' onChange={e => setTourImage(e.target.value)} />
+        <button type="button" className="AddEdit-btn" onClick={onClick}>Save</button>
       </div>
     </div>
   )
