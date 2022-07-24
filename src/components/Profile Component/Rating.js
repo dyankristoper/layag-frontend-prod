0785b7eb43a516b3a21ba3cc6cd1061b795a436d
Jarren <vince.dehonor1@gmail.com>
2022-07-25 07:03:18 +0800

    Feat: styling in modal for rating

--- src/components/Profile Component/Rating.js
+++ src/components/Profile Component/Rating.js
@@ -9,44 +9,48 @@ const Rating = () => {
 
   return (
     <div className="Rating">
-      <h3>Add a Rating</h3>
+      <div>
+        <img src="https://cdn-icons-png.flaticon.com/512/7644/7644639.png" alt="" />
+        <h3>Give us a feedback!</h3>
+      </div>
       <div className="Rating__details">
 
-        <div className="rating-information">
-            <label>Tour Name:</label>
-            <input type="text" readOnly />
+        <div className="Rating__details-name">
+          <label>Tour Name</label>
+          <input type="text" readOnly />
         </div>
 
-        <div className="rating-information">
-        <label>Rate:</label>
-        {[...Array(5)].map((star, i) => {
-          const ratingValue = i + 1;
-          return (
-            <>
-              <input
-                type="radio"
-                name="rating"
-                value={ratingValue}
-                onClick={() => setRating(ratingValue)}
-              />
-              <FaStar
-                size={25}
-                className="star"
-                color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
-                onMouseEnter={() => setHover(ratingValue)}
-                onMouseLeave={() => setHover(null)}
-              />
-            </>
-          );
-        })}
+        <label className="Rating__details-rate">Rate</label>
+        <div className="Rating__details-rating">
+
+          {[...Array(5)].map((star, i) => {
+            const ratingValue = i + 1;
+            return (
+              <>
+                <input
+                  type="radio"
+                  name="rating"
+                  value={ratingValue}
+                  onClick={() => setRating(ratingValue)}
+                />
+                <FaStar
+                  size={50}
+                  className="star"
+                  color={ratingValue <= (hover || rating) ? "#ffc107" : "#d2cedf"}
+                  onMouseEnter={() => setHover(ratingValue)}
+                  onMouseLeave={() => setHover(null)}
+                />
+              </>
+            );
+          })}
         </div>
 
-
-        <div>
-            <label>Comment:</label>
-            <textarea />
+        <div className="Rating__details-comment">
+          <textarea placeholder="Leave a comment..." />
         </div>
+        <button type="button" className="Rating__details-btn">Rate now</button>
       </div>
+      <h3 >Thank you!</h3>
     </div>
   );
 };
