2d5a09020063f5e77a95bfc343c538e7e522927f
John Christopher Azcarraga <dyankristoper@anonymous-mini.local>
2022-07-29 02:35:42 +0800

    Feat: Changed URL to BE prod

--- src/views/EditTour.js
+++ src/views/EditTour.js
@@ -24,11 +24,11 @@ const EditTour = () => {
       try {
         let response;
         if (id) {
-          response = await axios.put(`http://localhost:8000/api/v1/tours/${id}`, {
+          response = await axios.put(`https://layag-traveltours-be.herokuapp.com/api/v1/tours/${id}`, {
             name: tourName, duration: tourDuration, maxGroupSize: tourMaxGroupSize, difficulty: tourDifficulty, price: tourPrice, summary: tourSummary, description: tourDescription, imageCover: tourImage
           });
         } else {
-          response = await axios.post('http://localhost:8000/api/v1/tours', {
+          response = await axios.post('https://layag-traveltours-be.herokuapp.com/api/v1/tours', {
             name: tourName, duration: tourDuration, maxGroupSize: tourMaxGroupSize, difficulty: tourDifficulty, price: tourPrice, summary: tourSummary, description: tourDescription, imageCover: tourImage, locations: [], startLocation: ''
           });
         }
